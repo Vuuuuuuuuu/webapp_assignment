e83605c735b7d68942f5664c6f40b93868c200ea
Vuuuuuuuuu <99804407+Vuuuuuuuuu@users.noreply.github.com>
2025-07-04 21:03:57 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 from os import path
 import numpy as np
-#import matplotlib
+import matplotlib
 #matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import seaborn as sns
